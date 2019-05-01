c53dca044dbeeadf15f9e88dbe0991d217812f73
Simon Brady <simonbrady@mac.com>
2019-04-30 22:28:14 -0400

    componentize numerifyCall

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NumerifyCall from './components/numerifyCall.js';
+
+function App() {
+    return ( 
+      <div>
+        <NumerifyCall />
+      </div>
+    ); 
+    
+}
+
+export default App;
---
+++ src/components/numerifyCall.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class NumerifyCall extends React.Component {
+    render() { 
+      return <span > API call here < /span>
+    }
+}
+
+export default NumerifyCall;
