72c67cb31ee7d1738e53b1c0f16d265e7bc9f2c6
Simon Brady <simonbrady@mac.com>
2019-05-02 10:55:03 -0400

    add logo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import './App.css';
+import './assets/App.css';
 import NumerifyCall from './components/numerifyCall.js';
+import logo from './assets/logo-main.svg'
 
 class App extends Component {
     render() {
     return (
       <div class="main">
-        test 
-      </div>
-      <div>
-        <NumerifyCall />
+        <div class="logo-container">
+          <img src={logo} alt="logo"/>
+        </div>      
+        <div class="numerifyCall-container">
+          <NumerifyCall />
+        </div>
       </div>
     ); 
   } 
